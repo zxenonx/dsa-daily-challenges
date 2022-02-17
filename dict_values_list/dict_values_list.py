ae62565520eca7ea5d3251418b4e4d61c179c3f2
xzenon <forlanfr_8@hotmail.fr>
2022-02-17 22:37:24 +0100

    Add unique values from dict to list

---
+++ dict_values_list/dict_values_list.py
@@ -0,0 +1,15 @@
+"""
+Get all values from the dictionary and add them to a list but don’t add duplicates
+
+Given:
+
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+Expected Outcome:
+
+[47, 52, 44, 53, 54]
+"""
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44,
+         'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+print(list(set(speed.values())))
